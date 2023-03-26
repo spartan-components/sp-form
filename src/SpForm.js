87550685b9704887a8afbf64f65f06486d9c0286
timonforrer <timon.forrer@gmail.com>
2023-03-26 15:11:04 +0200

    Simplified code

--- src/SpForm.js
+++ src/SpForm.js
@@ -5,25 +5,16 @@ export class SpForm extends LitElement {
   static properties = {
     _form: {
       state: true
-    },
-    _inputs: {
-      state: true
-    },
-    _standardInputs: {
-      state: true
-    },
-    _groupedInputs: {
-      state: true
     }
-  };
+  }
 
   firstUpdated() {
 
     const slot = this.shadowRoot.querySelector('slot');
     const children = slot.assignedElements({ flatten: true });
-    this._form = children[0];
+    const form = children[0];
 
-    if (this._form.tagName !== 'FORM') {
+    if (form.tagName !== 'FORM') {
       throw new Error('Only form allowed as child');
     }
 
@@ -31,85 +22,66 @@ export class SpForm extends LitElement {
       throw new Error('Only one child allowed');
     }
 
-    this._form.setAttribute('novalidate', true);
+    this._form = form;
 
-    let inputs = this._form.querySelectorAll('input, textarea');
-    this._inputs = Array.from(inputs);
+    form.setAttribute('novalidate', true);
 
-    this._setupInputValidation(inputs);
+    let inputs = form.querySelectorAll('input, textarea');
 
-    this._form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      if (this._form.checkValidity()) {
-        this._form.requestSubmit();
+    inputs = Array.from(inputs);
+
+    inputs = inputs.map(input => {
+      const type = input.getAttribute('type');
+      let label;
+      if (type === 'radio') {
+        const parent = input.closest('fieldset');
+        label = parent.querySelector('legend');
       } else {
-        this._standardInputs.forEach(input => {
-          if(input.validity.valid === false) {
-            this.addErrorMessage({ input });
-          }
-        });
+        label = document.querySelector(`label[for=${input.id}]`);
+      }
+      return {
+        element: input,
+        label
       }
     });
-  }
 
-  _setupInputValidation() {
-
-    this._standardInputs = this._inputs.filter(input => (
-      ['text', 'email'].some(type => type === input.getAttribute('type'))
-      || input.nodeName === 'TEXTAREA'
-    ));
-
-    const radioInputs = this._inputs.filter(input => input.getAttribute('type') === 'radio');
-
-    const groupedInputs = {};
-
-    radioInputs.forEach(input => {
-      const key = input.getAttribute('name');
-      const oldValue = groupedInputs[key] || [];
-      groupedInputs[key] = [...oldValue, input];
-    });
-
-    this._groupedInputs = groupedInputs;
-
-    this._validateStandardInputs();
-  }
-
-  _validateStandardInputs() {
-    this._standardInputs.forEach(input => input.addEventListener('blur', event => {
+    inputs.forEach(input =>
+      input.element.addEventListener('blur', event => {
       const field = event.target;
       const { valid } = field.validity;
 
-      if (!valid) this.addErrorMessage({ input: field });
-      if (valid) this.removeErrorMessage({ input: field });
+      if (!valid) this.addErrorMessage({ input });
+      if (valid) this.removeErrorMessage({ input });
     }));
   }
 
   addErrorMessage({ input }) {
-    const isRadio = input.getAttribute('type') === 'radio';
-    const invalidationNode = isRadio ? input.getClosest('fieldset') : input;
-    const errorId = input.id + '-error';
+    this.removeErrorMessage({ input });
+    const { element, label } = input;
+    const isRadio = element.getAttribute('type') === 'radio';
+    const invalidationNode = isRadio ? element.closest('fieldset') : element;
+    const identifier = isRadio ? element.getAttribute('name') : element.id;
+    const errorId = `${identifier}-error`;
     invalidationNode.setAttribute('aria-invalid', true);
     invalidationNode.setAttribute('aria-describedby', errorId);
 
     const errorSpan = document.createElement('span');
     errorSpan.setAttribute('id', errorId);
-    errorSpan.innerText = input.validationMessage;
+    errorSpan.innerText = input.element.validationMessage;
 
-    if (!isRadio) {
-      input.insertAdjacentElement('beforebegin', errorSpan);
-    } else {
-      console.log('hello!');
-    }
+    label.insertAdjacentElement('afterend', errorSpan);
   }
 
   removeErrorMessage({ input }) {
-    const isRadio = input.getAttribute('type') === 'radio';
-    const invalidationNode = isRadio ? input.getClosest('fieldset') : input;
-    const errorId = input.id + '-error';
+    const { element } = input;
+    const isRadio = element.getAttribute('type') === 'radio';
+    const invalidationNode = isRadio ? element.closest('fieldset') : element;
+    const identifier = isRadio ? element.getAttribute('name') : element.id;
+    const errorId = `${identifier}-error`;
     invalidationNode.removeAttribute('aria-invalid');
     invalidationNode.removeAttribute('aria-describedby');
 
-    document.getElementById(errorId).remove();
+    this._form.querySelector(`#${errorId}`)?.remove();
   }
 
   render() {
